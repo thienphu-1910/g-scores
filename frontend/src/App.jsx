52434351ba99bf7f1ae6ffa8dbf08569e4dff18c
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2026-06-08 14:48:28 +0700

    chorse: create frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
